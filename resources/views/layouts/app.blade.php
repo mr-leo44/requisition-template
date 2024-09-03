38cbfd5fd9e2faad92b027d96dc2007782a46fdd
mr-leo44 <lionnelkaniki44@gmail.com>
2024-09-03 14:46:08 +0100

    add padding on bottom of app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
 </head>
 
 <body class="font-sans antialiased">
-    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <div class="min-h-screen bg-gray-100 dark:bg-gray-900 pb-8">
         @include('layouts.navigation')
 
         <!-- Page Heading -->
