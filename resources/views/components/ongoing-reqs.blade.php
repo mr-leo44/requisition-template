2c171b3311299965725e3bf9321af832c0e4eb0d
mr-leo44 <lionnelkaniki44@gmail.com>
2024-09-03 14:16:20 +0100

    toggle list or grid view

--- resources/views/components/ongoing-reqs.blade.php
+++ resources/views/components/ongoing-reqs.blade.php
@@ -1,5 +1,55 @@
 <div class="hidden p-2 rounded-lg" id="styled-ongoing" role="tabpanel" aria-labelledby="ongoing-tab">
-    <div class="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-3">
+    <div class="flex gap-3 justify-end items-center mb-6">
+        <div>
+            <div class="flex gap-2 justify-between items-center">
+                <button type="button" id="listView"
+                    class="p-2.5 ms-2 text-sm font-medium text-white bg-gray-400 hover:bg-gray-500 rounded-lg [&.active]:bg-gray-900">
+                    <svg class="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
+                            d="M9 8h10M9 12h10M9 16h10M4.99 8H5m-.02 4h.01m0 4H5" />
+                    </svg>
+                </button>
+                <button type="button" id="gridView"
+                    class="p-2.5 ms-2 text-sm font-medium text-white bg-gray-400 hover:bg-gray-500 rounded-lg [&.active]:bg-gray-900 active">
+                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M9.143 4H4.857A.857.857 0 0 0 4 4.857v4.286c0 .473.384.857.857.857h4.286A.857.857 0 0 0 10 9.143V4.857A.857.857 0 0 0 9.143 4Zm10 0h-4.286a.857.857 0 0 0-.857.857v4.286c0 .473.384.857.857.857h4.286A.857.857 0 0 0 20 9.143V4.857A.857.857 0 0 0 19.143 4Zm-10 10H4.857a.857.857 0 0 0-.857.857v4.286c0 .473.384.857.857.857h4.286a.857.857 0 0 0 .857-.857v-4.286A.857.857 0 0 0 9.143 14Zm10 0h-4.286a.857.857 0 0 0-.857.857v4.286c0 .473.384.857.857.857h4.286a.857.857 0 0 0 .857-.857v-4.286a.857.857 0 0 0-.857-.857Z" />
+                    </svg>
+                </button>
+            </div>
+        </div>
+        <div>
+            <form class="flex items-center max-w-sm mx-auto">
+                <label for="simple-search" class="sr-only">Search</label>
+                <div class="relative w-full">
+                    <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M3 5v10M3 5a2 2 0 1 0 0-4 2 2 0 0 0 0 4Zm0 10a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm12 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm0 0V6a3 3 0 0 0-3-3H9m1.5-2-2 2 2 2" />
+                        </svg>
+                    </div>
+                    <input type="text" id="simple-search"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-500 focus:border-orange-500 block w-full ps-10 p-2.5 dark:placeholder-gray-400 dark:text-white"
+                        placeholder="Rechercher..." required />
+                </div>
+                <button type="submit" class="p-2.5 ms-2 text-sm font-medium text-white bg-orange-500 rounded-lg">
+                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                    </svg>
+                    <span class="sr-only">Search</span>
+                </button>
+            </form>
+        </div>
+
+    </div>
+
+
+    <div class="grid md:grid-cols-2 lg:grid-cols-3 grid-cols-1 gap-3" id="cardGridView">
         <div
             class="block bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"">
             <div class=" border-b dark:border-gray-600 p-4">
@@ -11,8 +61,9 @@
                         deux
                         jours</span>
                 </div>
-                <p class="font-medium
-                                        text-md text-gray-700 dark:text-white">
+                <p
+                    class="font-medium
+                                                text-md text-gray-700 dark:text-white">
                     Sharon Jayes</p>
                 <p class="font-medium text-md text-gray-700 dark:text-white">Moyens
                     généraux</p>
@@ -56,8 +107,9 @@
                         deux
                         jours</span>
                 </div>
-                <p class="font-medium
-                                        text-md text-gray-700 dark:text-white">
+                <p
+                    class="font-medium
+                                                text-md text-gray-700 dark:text-white">
                     Sharon Jayes</p>
                 <p class="font-medium text-md text-gray-700 dark:text-white">Moyens
                     généraux</p>
@@ -101,8 +153,9 @@
                         deux
                         jours</span>
                 </div>
-                <p class="font-medium
-                                        text-md text-gray-700 dark:text-white">
+                <p
+                    class="font-medium
+                                                text-md text-gray-700 dark:text-white">
                     Sharon Jayes</p>
                 <p class="font-medium text-md text-gray-700 dark:text-white">Moyens
                     généraux</p>
@@ -146,8 +199,9 @@
                         deux
                         jours</span>
                 </div>
-                <p class="font-medium
-                                        text-md text-gray-700 dark:text-white">
+                <p
+                    class="font-medium
+                                                text-md text-gray-700 dark:text-white">
                     Sharon Jayes</p>
                 <p class="font-medium text-md text-gray-700 dark:text-white">Moyens
                     généraux</p>
@@ -191,8 +245,9 @@
                         deux
                         jours</span>
                 </div>
-                <p class="font-medium
-                                        text-md text-gray-700 dark:text-white">
+                <p
+                    class="font-medium
+                                                text-md text-gray-700 dark:text-white">
                     Sharon Jayes</p>
                 <p class="font-medium text-md text-gray-700 dark:text-white">Moyens
                     généraux</p>
@@ -236,8 +291,9 @@
                         deux
                         jours</span>
                 </div>
-                <p class="font-medium
-                                        text-md text-gray-700 dark:text-white">
+                <p
+                    class="font-medium
+                                                text-md text-gray-700 dark:text-white">
                     Sharon Jayes</p>
                 <p class="font-medium text-md text-gray-700 dark:text-white">Moyens
                     généraux</p>
@@ -281,8 +337,9 @@
                         deux
                         jours</span>
                 </div>
-                <p class="font-medium
-                                        text-md text-gray-700 dark:text-white">
+                <p
+                    class="font-medium
+                                                text-md text-gray-700 dark:text-white">
                     Sharon Jayes</p>
                 <p class="font-medium text-md text-gray-700 dark:text-white">Moyens
                     généraux</p>
@@ -326,8 +383,9 @@
                         deux
                         jours</span>
                 </div>
-                <p class="font-medium
-                                        text-md text-gray-700 dark:text-white">
+                <p
+                    class="font-medium
+                                                text-md text-gray-700 dark:text-white">
                     Sharon Jayes</p>
                 <p class="font-medium text-md text-gray-700 dark:text-white">Moyens
                     généraux</p>
@@ -371,8 +429,9 @@
                         deux
                         jours</span>
                 </div>
-                <p class="font-medium
-                                        text-md text-gray-700 dark:text-white">
+                <p
+                    class="font-medium
+                                                text-md text-gray-700 dark:text-white">
                     Sharon Jayes</p>
                 <p class="font-medium text-md text-gray-700 dark:text-white">Moyens
                     généraux</p>
@@ -406,4 +465,39 @@
             </div>
         </div>
     </div>
+    <div class="hidden text-gray-700 dark:text-white" id="cardListView">
+        List
+    </div>
 </div>
+</div>
+
+<script>
+    const listView = document.getElementById("listView");
+    const gridView = document.getElementById("gridView");
+    const cardGridView = document.getElementById("cardGridView");
+    const cardListView = document.getElementById("cardListView");
+
+    listView.addEventListener("click", toggleListView);
+    gridView.addEventListener("click", toggleGridView);
+
+    function toggleListView() {
+        if (!listView.classList.contains('active')) {
+            listView.classList.add("active");
+            cardListView.classList.remove("hidden");
+            cardListView.classList.add("active");
+            gridView.classList.remove("active");
+            cardGridView.classList.add("hidden");
+        }
+        
+    }
+
+    function toggleGridView() {
+        if (listView.classList.contains('active')) {
+            gridView.classList.add("active");
+            cardGridView.classList.remove("hidden");
+            cardGridView.classList.add("active");
+            listView.classList.remove("active");
+            cardListView.classList.add("hidden");
+        }
+    }
+</script>
